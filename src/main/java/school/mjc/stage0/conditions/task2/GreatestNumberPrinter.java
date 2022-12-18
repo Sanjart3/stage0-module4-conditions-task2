bde23a427802c1dd95a2ac0e2c3c0e90a2648161
Sanjar Totliboyev <2110110@newuzbekistanuniversity.uz>
2022-12-18 17:27:57 +0500

    just

--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        if (first==second) System.out.println(first+"\n"+second);
+        else System.out.println((first>second)?first:second);
     }
 }
